521f84bb41bde62ef3535bc73ca4cc1949d07f81
Arabi Kabir <mail2arabi@gmail.com>
2022-10-11 00:04:12 +0600

    hi3

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const http = require('http')
+const path = require('path')
 
-require('dotenv').config({ path: "./.env" })
+require('dotenv').config({ path: path.join(__dirname, ".env") })
 
 const app = require('./app')
 const { mongoConnect } = require('./src/services/mongo')
