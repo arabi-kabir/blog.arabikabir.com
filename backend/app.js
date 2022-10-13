c9515f8b9671ada307661eb72cdac17ab6746c1a
Arabi Kabir <mail2arabi@gmail.com>
2022-10-13 13:28:04 +0600

    size up

--- backend/app.js
+++ backend/app.js
@@ -5,9 +5,9 @@ const cors = require('cors')
 const app = express()
 
 app.use(express.static('./src/routes/post/uploads'))
-app.use(bodyparser.json())
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+// app.use(bodyparser.json())
+app.use(express.json({limit: "50mb", extended: true}))
+app.use(express.urlencoded({limit: "50mb", extended: true, parameterLimit: 5000000}))
 
 app.use(bodyparser.json())
 
