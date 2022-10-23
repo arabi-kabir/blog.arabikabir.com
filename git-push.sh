e805b8bf4e90275efd4199e7f10c3ac763f6b6cc
Arabi Kabir <mail2arabi@gmail.com>
2022-10-23 17:03:29 +0600

    git push changed

--- git-push.sh
+++ git-push.sh
@@ -2,11 +2,5 @@ echo "Enter your commit message"
 read message
 git add .
 git commit -m"${message}"
-# if [ -n "$(git status - porcelain)" ];
-# then
-#  echo "IT IS CLEAN"
-# else
-#  git status
- echo "Pushing data to remote server!!!"
- git push
-# fi
+echo "Pushing data to remote server!!!"
+git push
